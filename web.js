ef2531c7e1bd1fc6b15bd1f9de84fc62b42b0454
Rod Lai <rodwittlai@gmail.com>
2013-07-10 05:02:28 +0000

    trying hw3 part 1

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var content = fs.readFileSync("index.html");
+var content_msg = content.toString()
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content_msg);
 });
 
 var port = process.env.PORT || 5000;
